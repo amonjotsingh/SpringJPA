349874e1ad5037a6f6662059742e22da3faec8c9
chhinajot99@gmail.com <chhinajot99@gmail.com>
2022-04-22 19:11:24 -0400

    Student using spring JPA

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,13 +1,35 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Application.class, args);
     }
+
+//    @Bean
+//    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+//        return args -> {
+//            Student amonjot = new Student(
+//                    "Amonjot",
+//                    "Chhina",
+//                    "chhinajot@gmail.com",
+//                    26);
+//            studentRepository.save(amonjot);
+//            Student manu = new Student(
+//                    "Manu",
+//                    "Simriti",
+//                    "manu@gmail.com",
+//                    25);
+//            studentRepository.save(manu);
+//        };
+//
+//    }
 
 }
